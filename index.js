9f4933734aca223571d156ef93dfb088b3de56de
shinsato <shinsatoc@gmail.com>
2012-11-10 12:34:06 -0700

    loads header, index then footer

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 var fs = require('fs'),
 http = require('http');
 http.createServer(function (req, res) {
-  fs.readFile(__dirname + "/views/index.html", function (err,data) {
-    if (err) {
-      res.writeHead(404);
-      res.end(JSON.stringify(err));
-      return;
-    }
-    res.writeHead(200);
-    res.end(data);
-  });
+	var html = '';
 
+	html = fs.readFileSync(__dirname + "/views/header.html");
+	html = html + fs.readFileSync(__dirname + "/views/index.html");
+	html = html + fs.readFileSync(__dirname + "/views/footer.html");
+  res.writeHead(200);
+  res.end(html);
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
